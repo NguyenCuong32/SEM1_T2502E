37b6657ecff37007cc0f5522e51c0c26fa673566
ThanhTung247-st <tthanhtung247@gmail.com>
2025-02-28 19:56:56 +0700

    Update main.c

--- SEM1_T2502E/main.c
+++ SEM1_T2502E/main.c
@@ -1,84 +1,14 @@
-// Ví dụ minh hoạ về Loop (Vòng lặp)
+// Viết phương trình in các số theo thứ tự giảm dần
 
 #include <stdio.h>
-void forexample()
+void forexample(int n)
 {
-    printf("Begin loop \r\n");
-    for(int i = 0; i <= 5; i++)
-    {
-        printf("i = %d \r\n",i);
-    }
-    printf("End \r\n");
-}
-void forexamble2()
-{
-    printf("Begin \r\n");
-    printf("Hay nhap 1 so ");
-    int n;
-    scanf("%d",&n);
-    for(int i = 0; i <= n; i++)
-    {
-    printf("i = %d \r\n",i);
-    }
-}
-// Viết 1 phương trình in ra các số chẵn
-void forexample3()
-{
-    printf("Begin \r\n");
-    printf("Please enter a number : ");
-    int n;
-    scanf("%d",&n);
-    for(int i = 0; i <= n; i++)
-    {
-        if (i%2==0)
-        {
-            printf("i = %d \r\n",i);
-        }
-        else 
-        {
-            printf("Invalid \r\n");
-        }
-        
-    }
-}
-
-void forexample4()
-{
-    printf("Begin \r\n");
-    printf("Please enter a number : ");
-    int n;
-    scanf("%d",&n);
-    for(int i = 0; i <=n;)
-    {
-        printf("i = %d \r\n",i);
-        i = i + 2;
-    }
-}
-
-// Viết hàm với tham số
-void forexample5(int n)
-{
-    for(int i = 0; i <= n ;)
+    for( int i = n; i > 0;)
     {
         if(i%2==0)
         {
             printf("i = %d \r\n",i);
-            i = i +2;
-        }
-        else
-        {
-            printf("Invalid \r\n");
-        }
-    }
-}
-void forexample6(int m)
-{
-    for(int i = 0; i <= m;)
-    {
-        if(i%2==0)
-        {
-            printf("i = %d \r\n",i);
-            i = i + 2;
+            i = i - 2;
         }
         else
         {
@@ -88,18 +18,9 @@ void forexample6(int m)
 }
 int main()
 {
-    //forexample();
-    //forexamble2();
-    //forexample3();
-    //forexample4();
-    // printf("Please enter a number \r\n");
-    // int n;
-    // scanf("%d",&n);
-    // forexample5(n);
-    // printf("Please enter a number :");
-    int m;
-    scanf("%d",&m);
-    forexample6(m);
-    printf("Please enter a number :");
+    int n;
+    printf("Please enter a number : ");
+    scanf("%d",&n);
+    forexample(n);
     return 0;
 }
