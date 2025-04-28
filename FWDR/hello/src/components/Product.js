5ed5463e07f9c9ac5f96dab6b44edf35eb681fb5
greenwormstudio <greenwormstudio@gmail.com>
2025-04-28 18:09:46 +0700

    Demo code

---
+++ FWDR/hello/src/components/Product.js
@@ -0,0 +1,12 @@
+function Product() {
+  return <div>Product Component
+    <h1>Product</h1>
+    <p>This is the product page.</p>
+    <p>Here you can find all the products we offer.</p>
+    <p>We have a wide range of products to choose from.</p>
+    <p>We hope you find what you are looking for.</p>
+    <p>We are constantly updating our product line.</p>
+  </div>;
+
+}
+export default Product;
