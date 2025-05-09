b177c5105ba158eadeb82da9f856a7dd75fd33bd
greenwormstudio <greenwormstudio@gmail.com>
2025-05-09 19:35:47 +0700

    # complete call api

--- FWDR/eshop_project_2/src/components/EmployeeList.js
+++ FWDR/eshop_project_2/src/components/EmployeeList.js
@@ -1,9 +1,30 @@
 import Employee from "./Employee";
-import employees from "../data/Employee.json";
+// import employees from "../data/Employee.json";
 import axios from "axios"
 import { useEffect, useState } from "react"
 function EmployeeList() {
 
+    const [employees,setEmployees] = useState([]);
+    const [loading,setLoading] = useState(true);
+    const [error,setError] = useState(null);
+
+    useEffect(()=>{
+        axios.get("https://650d7ae9a8b42265ec2c4713.mockapi.io/api/demo/employee")
+        .then((response)=>{
+            setEmployees(response.data);// Kết quả trả vể là một mảng employee. biến employees sẽ có giá trị là mảng employee
+            setLoading(false);// Biến loading có giá trị là false
+        })
+        .catch((error)=>{
+            setError(error); // Biến error là thông báo về lỗi nếu có.
+            setLoading(false);// Biến loading có giá trị là false
+        })
+    })
+    if(loading===true){
+        return <div>Loading...</div>
+    }
+    if(error){
+        return <div>Error: {error.message}</div>
+    }
     return (
         <div>
             {employees.map(employee => (
