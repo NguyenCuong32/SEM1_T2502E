9a1cdc26605356605499d76f0a68e25f10636d25
greenwormstudio <greenwormstudio@gmail.com>
2025-05-21 18:45:58 +0700

    Create Database with PK, FK

--- DMS/Demo.sql
+++ DMS/Demo.sql
@@ -1,27 +1,37 @@
+-- Tạo mới Database
 CREATE DATABASE HR_Database
 GO
+-- Làm việc với Database mong muốn
 USE HR_Database
 GO
-CREATE TABLE Employee(
-    Emp_No INT,
-    Emp_Name NVARCHAR(255),
-    Grade CHAR(1),
-    PRIMARY KEY (Emp_No)
-)
-
-GO
-CREATE TABLE Project(
-    Project_Id INT,
-    Project_Name NVARCHAR(60)
-)
-GO 
+-- Tạo mới bảng Grade
 CREATE TABLE Grade(
     Grade_Id CHAR(1),
     Salary DECIMAL(6,2)
+    PRIMARY KEY(Grade_Id) -- Đặt Grade_Id làm khoá chính
 )
+-- Tạo mới bảng Employee
+CREATE TABLE Employee(
+    Emp_No INT,-- Trường dữ liệu và kiểu dữ liệu 
+    Emp_Name NVARCHAR(255),
+    Grade_Id CHAR(1),
+    PRIMARY KEY (Emp_No), -- Đặt Emp_No làm khoá chính của bảng Employee
+    FOREIGN KEY (Grade_Id) REFERENCES Grade(Grade_Id)
+)
+
+GO
+-- Tạo mới bảng Project
+CREATE TABLE Project(
+    Project_Id INT,
+    Project_Name NVARCHAR(60)
+    PRIMARY KEY(Project_Id) -- Đặt Project_Id làm khoá chính
+)
+GO 
+
 GO
 CREATE TABLE Employee_Project(
     Emp_No INT,
     Project_Id INT
+    PRIMARY KEY(Emp_No,Project_Id) -- Đặt Emp_No và Project_Id làm khoá chính
 )
 
